53d7acf276eaf4fa6cbd47c6ca21303e80bf7737
atabe <atabekgithub23>
2022-05-13 17:10:42 +0600

    six

--- build/index.js
+++ build/index.js
@@ -1,7 +1,13 @@
+"use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
 const NumbersCollection_1 = require("./NumbersCollection");
+const CharactersCollection_1 = require("./CharactersCollection");
 const numbersCollection = new NumbersCollection_1.NumbersCollection([4, -3, -2, 0, 11]);
 const sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+const charactersCollection = new CharactersCollection_1.CharactersCollection('AtabekAdilbe');
+const sorter1 = new Sorter_1.Sorter(charactersCollection);
+sorter1.sort();
+console.log(charactersCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
 const numbersCollection = new NumbersCollection([4 , -3 , -2 , 0 ,11])
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+
+
+const charactersCollection = new CharactersCollection('AtabekAdilbe');
+const sorter1 = new Sorter(charactersCollection);
+sorter1.sort();
+console.log(charactersCollection.data);
