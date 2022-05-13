963dfc1326f45ab918569f2e1b7b1500491b18e3
atabe <atabekgithub23>
2022-05-13 16:43:16 +0600

    five

--- build/index.js
+++ build/index.js
@@ -1,2 +1,7 @@
-"use strict";
-console.log('hi');
+Object.defineProperty(exports, "__esModule", { value: true });
+const Sorter_1 = require("./Sorter");
+const NumbersCollection_1 = require("./NumbersCollection");
+const numbersCollection = new NumbersCollection_1.NumbersCollection([4, -3, -2, 0, 11]);
+const sorter = new Sorter_1.Sorter(numbersCollection);
+sorter.sort();
+console.log(numbersCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('hi');
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
+
+const numbersCollection = new NumbersCollection([4 , -3 , -2 , 0 ,11])
+const sorter = new Sorter(numbersCollection);
+sorter.sort();
+console.log(numbersCollection.data);
