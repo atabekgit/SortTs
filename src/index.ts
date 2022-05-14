af1efa5d89d6d322b7f59208ac7c5156ae03cc85
atabe <atabekgithub23>
2022-05-14 10:38:49 +0600

    seven

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
-const numbersCollection = new NumbersCollection([4 , -3 , -2 , 0 ,11])
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+const numbersCollection = new NumbersCollection([4 , -300, -2 , 0 ,11])
+numbersCollection.sort();
 console.log(numbersCollection.data);
 
 
-const charactersCollection = new CharactersCollection('AtabekAdilbe');
-const sorter1 = new Sorter(charactersCollection);
-sorter1.sort();
+ const charactersCollection = new CharactersCollection('hsbdsbdchshfdsUscskdSDJH');
+charactersCollection.sort();
 console.log(charactersCollection.data);
+
+
+const linkedList = new LinkedList();
+linkedList.add(100);
+linkedList.add(-11);
+linkedList.add(-3);
+linkedList.add(7);
+
+linkedList.sort();
+linkedList.print();
